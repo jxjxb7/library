106ca4b67b22f3f475f4a97f661ed74e59e184a2
jxjxb7 <153766702+jxjxb7@users.noreply.github.com>
2024-06-26 23:20:51 +0300

    Update library.py

--- library.py
+++ library.py
@@ -27,36 +27,47 @@ class Readers(Base):
     __tablename__ = "readers"
 
     id = Column(Integer, primary_key=True, index=True)
-    reader_name = Column(String(50), index=True)
-    surname = Column(String(50), index=True)
-    patronymic = Column(String(50), index=True)
-    address = Column(String(50), index=True)
-    phone = Column(String(11), index=True)
+    reader_name = Column(String(50))
+    surname = Column(String(50))
+    patronymic = Column(String(50))
+    address = Column(String(50))
+    phone = Column(String(11))
+    
+    issuance = relationship("Issuance", back_populates="reader")
 
 class Issuance(Base):
     __tablename__ = "issuance"
 
-    id = Column(Integer, primary_key=True, index=True)
-    book_cipher = Column(String(30), index=True)
-    date_of_issue = Column(String(50), index=True)
-    signature = Column(String(50), index=True)
+    id = Column(Integer, ForeignKey('reader.id'), primary_key=True, index=True)
+    book_cipher = Column(String(30), ForeignKey('books.id'), primary_key=True, index=True)
+    date_of_issue = Column(DateTime)
+    signature = Column(String(50))
+    
+    reader = relationship("Readers", back_populates="issuance")
+    book = relationship("Books", back_populates="issuance")
 
 class Publishing(Base):
     __tablename__ = "publishing"
 
     id = Column(Integer, primary_key=True, index=True)
-    publishing_name = Column(String(100), index=True)
-    publishing_city = Column(String(50), index=True)
+    publishing_name = Column(String(100))
+    publishing_city = Column(String(50))
+    
+    books = relationship("Books", back_populates="publishing")
 
 class Books(Base):
     __tablename__ = "books"
 
     book_cipher = Column(Integer, primary_key=True, index=True)
-    book_name = Column(String(50), index=True)
-    first_author = Column(String(50), index=True)
-    year_of_publishing = Column(Integer, index=True)
-    book_price_rub = Column(Integer, index=True)
-    copies = Column(Integer, index=True)
+    book_name = Column(String(50))
+    first_author = Column(String(50))
+    year_of_publishing = Column(Integer)
+    book_price_rub = Column(Integer)
+    copies = Column(Integer)
+    
+    publishing_id = Column(Integer, ForeignKey('Publishing.publishing_id'))
+    publishing = relationship("Publishing", back_populates="books")
+    issuance = relationship("Issuance", back_populates="book")
 
 
 # Создание таблиц в базе данных
@@ -71,9 +82,6 @@ class UserResponse(BaseModel):
     id: int
     name: str
     email: str
-
-    class Config:
-        orm_mode = True
 
 class BooksCreate(BaseModel):
     book_cipher: int
@@ -92,10 +100,7 @@ class BooksResponse(BaseModel):
     book_price_rub: int
     copies: int
 
-    class Config:
-        orm_mode = True
-
-
+  
 class PublishingCreate(BaseModel):
     publishing_name: str
     publishing_city: str
@@ -105,22 +110,19 @@ class PublishingResponse(BaseModel):
     publishing_name: str
     publishing_city: str
 
-    class Config:
-        orm_mode = True
-
+    
 class IssuanceCreate(BaseModel):
     book_cipher: int
-    date_of_issue = str
-    signature = str
+    date_of_issue: datetime.datetime
+    signature: str
 
 class IssuanceResponse(BaseModel):
     id: int
     book_cipher: int
-    date_of_issue = str
-    signature = str
+    date_of_issue: str
+    signature: str
 
-    class Config:
-        orm_mode = True
+
 
 class ReadersCreate(BaseModel):
     reader_name: str
@@ -158,6 +160,7 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
+
 # Маршрут для создания нового пользователя
 @app.post("/users/", response_model=UserResponse)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
@@ -170,3 +173,69 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     except IntegrityError:
         db.rollback()
         raise HTTPException(status_code=400, detail="Email already registered")
+
+
+@app.get('/books/', response_model=BooksResponse)
+def read_books(db: Session = Depends(get_db)):
+    return db.query(Books).all()
+
+
+@app.put('/books/{book_id}', response_model=BooksResponse)
+def update_book(book_id: int, book: Books, db: Session = Depends(get_db)):
+    db_books = db.query(Books).filter(Books.id == book_id).first()
+    if db_books is None:
+        raise HTTPException(status_code=404, detail="Book not found")
+    for var, value in vars(book).items():
+        setattr(db_books, var, value) if value else None
+    db.commit()
+    db.refresh(db_books)
+    return db_books
+
+
+@app.post('/books/', response_model=BooksResponse)
+def create_book(book: Books, db: Session = Depends(get_db)):
+    db_book = Books(**book.dict())
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_books)
+    return db_books
+
+
+@app.put("/publishing/{publishing_id}/", response_model=PublishingResponse)
+def update_publishing(publishing_id: int, publishing: PublishingCreate, db: Session = Depends(get_db)):
+    db_publishing = db.query(Publishing).filter(Publishing.id == publishing_id).first()
+    if db_publishing is None:
+        raise HTTPException(status_code=404, detail="Publishing not found")
+    for var, value in vars(publishing).items():
+        setattr(db_publishing, var, value) if value else None
+    db.commit()
+    db.refresh(db_publishing)
+    return db_publishing
+
+
+@app.get("/publishing/", response_model=PublishingResponse)
+def get_publishers_with_books(db: Session = Depends(get_db)):
+    publishing = db.query(Publishing).join(Books).distinct().all()
+    return publishing
+
+@app.post("/publishing/", response_model=PublishingResponse)
+def create_publishing(publisher: PublishingCreate, db: Session = Depends(get_db)):
+    db_publishing = Publishing(**publishing.dict())
+    db.add(db_publishing)
+    db.commit()
+    db.refresh(db_publishing)
+    return db_publishing
+
+@app.get("/readers/{reader_id}", response_model=ReadersResponse)
+def read_readers(reader_id: int, db: Session = Depends(get_db)):
+    reader = db.query(Readers).filter(Readerse.reader_id == reader_id).first()
+    if reader is None:
+        raise HTTPException(status_code=404, detail="Reader not found")
+    return reader
+
+@app.get("/readers/overdue/", response_model=ReadersResponse)
+def read_readers_with_overdue_books(db: Session = Depends(get_db)):
+    overdue_books = db.query(Issuance).filter(Issuance.return_date < date.today()).all()
+    readers = [issuance.reader for issuance in overdue_books]
+    return readers
+
